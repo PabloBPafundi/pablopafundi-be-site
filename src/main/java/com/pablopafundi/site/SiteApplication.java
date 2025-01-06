7ac948185e07662a4382ce9f22be1edab4e1c1fb
PabloBPafundi <128729717+PabloBPafundi@users.noreply.github.com>
2025-01-06 12:41:02 -0300

    commit

--- src/main/java/com/pablopafundi/site/SiteApplication.java
+++ src/main/java/com/pablopafundi/site/SiteApplication.java
@@ -17,19 +17,4 @@ public class SiteApplication {
 	public static void main(String[] args) {SpringApplication.run(SiteApplication.class, args);}
 
 
-	/*
-	@Bean
-	CommandLineRunner user(UserService userService, MyProfileService myProfileService) {
-		return args -> {
-			if (!userService.userExistsByUserName("admin")) {
-				userService.createUser("admin", "admin_password", Role.ADMIN);
-				System.out.println("Usuario 'admin' creado correctamente.");
-			} else {
-				System.out.println("El usuario 'admin' ya existe.");
-			}
-
-		};
-
-
-	}*/
 }
